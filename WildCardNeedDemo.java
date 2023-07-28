c1c4c403502e0ec0eaa54d89df1354b0eb20635a
monari1 <francisnyasinga@gmail.com>
2023-07-28 20:50:47 +0300

    wildcard main

--- WildCardNeedDemo.java
+++ WildCardNeedDemo.java
@@ -1,9 +1,16 @@
 public class WildCardNeedDemo {
     public static void main(String[] args) {
+            GenericStack<Integer> inStack = new GenericStack<>();
+            inStack.push(1);
+            inStack.push(2);
+            inStack.push(-2);
+        System.out.println("The max number is " + max(inStack));
+
+
 
     }
 
-    public static double max(GenericStack<Number> stack){
+    public static double max(GenericStack<? extends Number> stack){
         double max = stack.pop().doubleValue();
         while (!stack.isEmpty()){
             double value = stack.pop().doubleValue();
