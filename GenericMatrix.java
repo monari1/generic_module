0c18585061defcb45d424eeabb4c3a5ff0cbae18
monari1 <francisnyasinga@gmail.com>
2023-08-04 21:34:03 +0300

    generic class instatatiation

---
+++ GenericMatrix.java
@@ -0,0 +1,8 @@
+public abstract class GenericMatrix <E extends Number> {
+    protected  abstract E add(E o1, E o2);
+    protected  abstract E multiply(E o1, E o2);
+    protected  abstract  E zero();
+
+
+
+}
